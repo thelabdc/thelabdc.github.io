0a3c76363635ab3ed454803a220e95f3e4f57ef1
Kevin Wilson <khwilson@gmail.com>
2019-01-04 17:29:32 -0500

    Move to gulp 4
    And while we're here, tidy up some dependencies.
    
    Note that gulp-gh-page's maintainer has updated the things we were
    relying on the `tekd` repo for, so we're switching back to that.
    However, note that they haven't redeployed it to npm yet, so we're
    setting the version in our package.json for now.

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,37 @@
-var gulp            = require('gulp'),
-    shell           = require('gulp-shell'),
-    ghPages         = require('gulp-gh-pages'),
-    imagemin        = require('gulp-imagemin'),
-    cp              = require('child_process'),
-    runSequence     = require('run-sequence').use(gulp);
+const gulp = require('gulp')
+const imagemin = require('gulp-imagemin')
+const cp = require('child_process')
 
-var messages = {
-    jekyllBuild: 'building...'
-};
-
-gulp.task('image', function () {
+const image = () => {
   return gulp.src('src/images/**/*')
-    .pipe(imagemin())
-    .pipe(gulp.dest('_site/images'));
-});
+      .pipe(imagemin())
+      .pipe(gulp.dest('_site/images'))
+}
 
-gulp.task('contentful', shell.task(['bundle exec jekyll contentful']));
+const contentful = () => cp.spawn('bundle', ['exec', 'jekyll', 'contentful'])
 
-gulp.task('circleci', function () {
+const circleci = () => {
   return gulp.src('.circleci/*')
-    .pipe(gulp.dest('_site/.circleci'));
-});
+    .pipe(gulp.dest('_site/.circleci'))
+}
 
-gulp.task('push-gh-source', shell.task(['git push origin source']));
+const pushGHSource = () => cp.spawn('git', ['push', 'origin', 'source'])
 
-gulp.task('push-gh-pages', function () {
+const pushGHPages = () => {
   return gulp.src('_site/**/*')
-    .pipe(ghPages({ force: true, branch: 'master' }));
-});
+             .pipe(ghPages({ force: true, branch: 'master' }))
+}
 
-gulp.task('jekyll', shell.task(['bundle exec jekyll build --incremental']));
+const build = () => cp.spawn('bundle', ['exec', 'jekyll', 'build'])
 
-gulp.task('build:prod', shell.task(['bundle exec jekyll build']));
+const deploy = gulp.series(contentful, build, image, circleci, pushGHSource, pushGHPages)
 
-gulp.task('deploy', function (callback) {
-  runSequence(
-    'contentful',
-    'build:prod',
-    'image',
-    'circleci',
-    'push-gh-source',
-    'push-gh-pages',
-    callback
-  );
-});
+module.exports = {
+  circleci,
+  contentful,
+  image,
+  pushGHSource,
+  pushGHPages,
+  build,
+  deploy
+}
